46e43b97a65d3e884eae74a25f5ad3b1393c9db5
1johnwick001 <john.wick00000456@gmail.com>
2024-09-05 14:46:39 +0530

    with extended size in server side

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,10 +21,11 @@ connectDB();
 const app = express()
 
 //middlewares
-app.use(express.json());
+app.use(express.json({ limit: '50mb' })); 
 // middleware to parse url-encoded form data
 app.use(express.urlencoded({
-    extended:true
+    extended:true,
+    limit: '50mb'
 }));
 app.use(cors());
 app.use(cookieParser());
